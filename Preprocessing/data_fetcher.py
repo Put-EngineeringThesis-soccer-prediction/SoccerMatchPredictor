4d3bcad178d6daa7193df9f3d8307f0f3613c292
Arkadiusz Chmura <arekchmura@gmail.com>
2020-12-12 19:00:49 +0100

    Make use of cache

--- Preprocessing/data_fetcher.py
+++ Preprocessing/data_fetcher.py
@@ -10,12 +10,16 @@ class DataFetcher:
 
     def fetch_data_for_season(self, season, params):
         url = get_url_for_matches_in_season(season.value, params)
+        if url in self.cache:
+            return self.cache[url]
         data = requests.get(url).text
         self.cache[url] = data
         return data
 
     def fetch_data_for_team_in_season(self, season, team, params):
         url = get_url_for_team_matches_in_season(season.value, team.value, params)
+        if url in self.cache:
+            return self.cache[url]
         data = requests.get(url).text
         self.cache[url] = data
         return data
