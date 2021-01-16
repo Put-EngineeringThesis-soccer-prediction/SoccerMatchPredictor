fd71245d7f41052137332e57849905e87e2ff1ed
Bartosz Przybył <38264818+BartekPrz@users.noreply.github.com>
2021-01-16 17:32:10 +0100

    Add algorithms to project (#4)
    * Checkpoint - before multi-imbalance
    
    * some changes in algorithm approach
    
    * some changes in interactive func
    
    * Fixing dataset spliting with globalCS
    
    * cleaun up notebooks
    
    * Add algorithms to project
    
    Co-authored-by: Iwo <iwo.naglik98@gmail.com>

---
+++ Algorithms/split_block.py
@@ -0,0 +1,20 @@
+from dataset_operation import DatasetOp
+
+class BlockingSplit(DatasetOp):
+    """This class splits dataset into blocks from one margin to next margin and make blocks as consecutive ones."""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def generate_cv(self, X, y):
+        self._compute_param_dict(len(X))
+        
+        for epoch in range(self.n_blocks):
+            margin = epoch * self.param_info_['train_size']
+            train_indices = range(margin, margin + self.param_info_['train_size'])
+            if epoch == self.n_blocks - 1:
+                end = len(X)
+            else:
+                end = margin + self.param_info_['train_size'] + self.param_info_['test_size']
+            test_indices = range( margin + self.param_info_['train_size'], end)
+            
+            yield list(train_indices), list(test_indices)
